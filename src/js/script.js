75d7bdd29c4e6396f1e1b04cfa80e692255271f4
Kuba1507 <kuba.romanski@onet.com.pl>
2023-12-21 13:14:53 +0100

    added script for year control in footer

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ const navbar = document.querySelector(".nav-bar");
 const navItemsList = document.querySelector(".nav-bar__list");
 const header = document.querySelector(".header");
 const navlinks = document.querySelectorAll(".navlink");
+const actualYear = document.querySelector(".year");
 
 const handleNav = () => {
 	navbar.classList.toggle("show-nav");
@@ -14,7 +15,13 @@ const closeNav = () => {
 	navItemsList.classList.remove("show-list");
 };
 
+const setCurrentYear = () => {
+	const currentYear = new Date().getFullYear();
+	actualYear.textContent = currentYear;
+};
+
 navlinks.forEach((link) => {
 	link.addEventListener("click", closeNav);
 });
 burgerBtn.addEventListener("click", handleNav);
+window.addEventListener("load", setCurrentYear);
