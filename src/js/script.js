8f51c47761ce7b9db66fdbbf2a6c1c666fa14a97
Kuba1507 <kuba.romanski@onet.com.pl>
2023-12-21 11:15:20 +0100

    fixed nav

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,20 @@
-const burgerBtn = document.querySelector('.hamburger')
-const navbar = document.querySelector('.nav-bar')
-const navItemsList = document.querySelector('.nav-bar__list')
-const header = document.querySelector('.header')
+const burgerBtn = document.querySelector(".hamburger");
+const navbar = document.querySelector(".nav-bar");
+const navItemsList = document.querySelector(".nav-bar__list");
+const header = document.querySelector(".header");
+const navlinks = document.querySelectorAll(".navlink");
 
 const handleNav = () => {
-	navbar.classList.toggle('show-nav')
-	navItemsList.classList.toggle('show-list')
-}
+	navbar.classList.toggle("show-nav");
+	navItemsList.classList.toggle("show-list");
+};
 
-burgerBtn.addEventListener('click', handleNav)
+const closeNav = () => {
+	navbar.classList.remove("show-nav");
+	navItemsList.classList.remove("show-list");
+};
 
+navlinks.forEach((link) => {
+	link.addEventListener("click", closeNav);
+});
+burgerBtn.addEventListener("click", handleNav);
